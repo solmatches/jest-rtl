b6bddf05c4cbe089dceaddff2be003dca024a860
Mika <solinepik@gmail.com>
2022-04-04 22:52:07 +0900

    feat: 테스트코드에 맞춰 체크박스 구현

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import { useState } from "react";
 const initialColor = "red";
 
 function App() {
+	const [disabled, setDisabled] = useState(false);
 	const [buttonColor, setButtonColor] = useState(initialColor);
 	const newButtonColor = buttonColor === initialColor ? "blue" : initialColor;
+
+	const handleButtonDisabled = (event) => {
+		setDisabled(event.target.checked);
+	};
+
 	return (
 		<div>
 			<button
+				disabled={disabled}
 				style={{ backgroundColor: buttonColor }}
 				onClick={() => setButtonColor(newButtonColor)}>
 				Change to {newButtonColor}
 			</button>
+			<input
+				type="checkbox"
+				checked={disabled}
+				onChange={handleButtonDisabled}
+			/>
 		</div>
 	);
 }
