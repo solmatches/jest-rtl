6c4d37c7d60a36c481c7f59c80d429057e342fee
Mika <solinepik@gmail.com>
2022-04-05 23:52:34 +0900

    feat: 버튼 활성화, 색상 시뮬레이션 코드 우선 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("button has correct initial color", () => {
@@ -38,4 +39,29 @@ test("Checkbox disables button on first click and enables on second click", () =
 
 	fireEvent.click(checkbox);
 	expect(colorButton).toBeEnabled();
+});
+
+test("버튼이 비활성화되면 회색이되고 활성화되면 붉은색으로 변한다. 그리고 버튼 색상이 바뀌고 버튼이 비활성화 되면 다시 버튼은 회색. 버튼을 활성화 시키면 버튼은 파란색이다.", () => {
+	render(<App />);
+
+	const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+	const colorButton = screen.getByRole("button", { name: "Change to blue" });
+	const user = userEvent.setup();
+
+	user.click(checkbox);
+	expect(colorButton).toBeDisabled();
+	expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
+
+	user.click(checkbox);
+	expect(colorButton).toBeEnabled();
+	expect(colorButton).toHaveStyle({ background: "red" });
+
+	user.click(colorButton);
+	user.click(checkbox);
+	expect(colorButton).toBeDisabled();
+	expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
+
+	user.click(checkbox);
+	expect(colorButton).toBeEnabled();
+	expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
 });
