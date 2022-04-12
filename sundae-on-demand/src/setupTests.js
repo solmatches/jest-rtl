68c5e05b3e8ac16f530b28efc30792653a8ba1ed
Mika <solinepik@gmail.com>
2022-04-12 23:02:49 +0900

    feat: Mock service worker 설정

--- sundae-on-demand/src/setupTests.js
+++ sundae-on-demand/src/setupTests.js
@@ -2,4 +2,16 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { server } from "./mocks/server";
+
+// 테스트가 시작하기전에 API mocking 설정
+beforeAll(() => server.listen());
+
+// 테스트 중에 추가할 수 있는 모든 request handlers를 설정하여
+// 다른 테스트에 영향을 미치지 않도록 한다
+afterEach(() => server.resetHandlers());
+
+// 테스트 종료 후 정리
+afterAll(() => server.close());
