f59bc114be66b2be51f7502ff71205b4e8752af9
Mika <solinepik@gmail.com>
2022-04-23 22:37:23 +0900

    feat: 주문 단계 통합 테스트 케이스 작성

---
+++ sundae-on-demand/src/tests/orderPhase.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import App from "../App";
+
+test('올바른 주문 단계', () => {
+    // 앱이 렌더되면 
+
+    // 아이스크립 스쿱과 토핑이 추가되고
+
+    // 주문 버튼을 클릭
+
+    // 주문 정보 요약을 확인
+
+    // 이용 약관에 동의하고 버튼을 클릭하여 주문 확인
+
+    // 확인 페이지에서 주문번호 확인
+
+    // 확인 페이지에서 새로운 주문 버튼을 클릭
+
+    // 스쿱과 토핑 소계가 재설정 되었는지 확인
+})
