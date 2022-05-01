35981cc20f079f2b84dcabc475ae2022b49c388f
Mika <solinepik@gmail.com>
2022-05-01 23:51:14 +0900

    feat: 토핑을 선택하지 않은 경우 테스트 작성

--- sundae-on-demand/src/tests/orderPhase.test.jsx
+++ sundae-on-demand/src/tests/orderPhase.test.jsx
@@ -89,19 +89,74 @@ test("올바른 주문 단계", async () => {
 	await screen.findByRole("checkbox", { name: "Cherries" });
 });
 
-test("토핑을 선택하지 않은 경우", () => {
+test("토핑을 선택하지 않은 경우", async () => {
 	render(<App />);
 
+	const user = userEvent.setup();
 	// 토핑을 주문하지 않은 경우 orderSummary 페이지에서 토핑을 제거한다.
 	// 아이스크림 스쿱 선택
+	const ChocolateScoop = screen.getByRole("spinbutton", {
+		name: "Chocolate",
+	});
+	await user.clear(ChocolateScoop);
+	await user.type(ChocolateScoop, "2");
+
+	expect(ChocolateScoop).toHaveValue(2);
+
 	// 주문하기 클릭
+	const orderButton = screen.getByRole("button", { name: "주문하기" });
+
+	expect(orderButton).toBeInTheDocument();
+
+	await user.click(orderButton);
+
 	// 토핑이 표시되지 않은 주문 요약 확인
+	const scoopTotal = screen.getByRole("heading", { name: "Scoops: 4,000원" });
+	expect(scoopTotal).toBeInTheDocument();
+
+	const toppingTotal = screen.queryByRole("heading", {
+		name: "Toppings: 0원",
+	});
+	expect(toppingTotal).not.toBeInTheDocument();
+
 	// 이용약관에 동의하고 주문 확인 버튼 클릭
+	const agreeCheckbox = screen.getByRole("checkbox", { name: "이용약관" });
+	await user.click(agreeCheckbox);
+	expect(agreeCheckbox).toBeChecked();
+
 	// 로딩 표시
+	const loading = screen.getByText(/loading/i);
+	expect(loading).toBeInTheDocument();
+
 	// 주문 완료 페이지 나타남
+	const thankYouPage = await screen.findByRole("heading", {
+		name: "감사합니다!",
+	});
+	expect(thankYouPage).toBeInTheDocument();
+
 	// 로딩 표시 제거
+	const notLoading = screen.queryByText("loading");
+	expect(notLoading).not.toBeInTheDocument();
+
 	// 주문 번호 확인
-	// 제주문 버튼 클릭
+	const orderNumber = await screen.findByText("주문번호");
+	expect(orderNumber).toBeInTheDocument();
+
+	// 재주문 버튼 클릭
+	const newOrderButton = await screen.findByRole("button", {
+		name: "재주문",
+	});
+	expect(newOrderButton).toBeInTheDocument();
+
+	await user.click(newOrderButton);
+
 	// 주문 페이지가 초기화 되었는지 확인
+	const initScoopTotal = screen.getByText("Scoops 합계: 0원");
+	expect(initScoopTotal).toBeInTheDocument();
+
 	// 메뉴들이 잘 불러와졌는지 확인.
+	const initChocolateScoop = screen.getByRole("spinbutton", {
+		name: "Chocolate",
+	});
+	expect(initChocolateScoop).toBeInTheDocument();
 });
